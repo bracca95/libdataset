5c53932f1b47f2612abc5509cea90077746daf52
lorenzo <lbraccaioli.falcone@gmail.com>
2023-05-04 15:11:02 +0200

    inherit from abstract dataset

--- src/datasets/dataset_utils.py
+++ src/datasets/dataset_utils.py
@@ -1,22 +1,34 @@
 from typing import Optional
 from dataclasses import dataclass
-from torch.utils.data import Dataset, Subset
+from torch.utils.data import Subset
 
 from src.utils.config_parser import Config
+from src.datasets.staple_dataset import CustomDataset
 from src.datasets.defectviews import DefectViews, BubblePoint
 
 
 @dataclass
 class SubsetInfo:
+    """SubsetInfo dataclass
+
+    The aim of this class is to wrap the output of torch.utils.data.random_split, so that it contains the information
+    related to the number of samples that belong to the split subsets. If you are creating your own dataset and then
+    you want to use pytorch's random_split method, this is the way to go not to lose information.
+
+    name (str): name of the split set { 'train', 'val', 'test' }
+    subset (Optional[Subset]): one element of the list outputted by torch.utils.data.random_split
+    info_dict (Optional): dict with (key, val) = (class name, number of instances per class)
+    """
+    
     name: str
     subset: Optional[Subset]
     info_dict: Optional[dict]
 
 
-class CustomDatasetLoader:
+class DatasetBuilder:
 
     @staticmethod
-    def load_dataset(config: Config) -> Dataset:
+    def load_dataset(config: Config) -> CustomDataset:
         if config.dataset_type == "all":
             return DefectViews(config.dataset_path, config.augment_offline, config.augment_online, config.crop_size)
         elif config.dataset_type == "binary":
---
+++ src/datasets/staple_dataset.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing import Optional, List
+from torch.utils.data import Dataset
+
+
+class CustomDataset(ABC, Dataset):
+
+    @abstractmethod
+    def get_image_list(self, filt: Optional[List[str]]) -> List[str]:
+        ...
+
+    @abstractmethod
+    def get_label_list(self) -> List[int]:
+        ...
+
+    def __getitem__(self, index):
+        return super().__getitem__(index)
+    
+    # def __len__(self):
+        """ https://github.com/pytorch/pytorch/issues/25247#issuecomment-525380635
+        No `def __len__(self)` default?
+        See NOTE [ Lack of Default `__len__` in Python Abstract Base Classes ]
+        in pytorch/torch/utils/data/sampler.py 
+        """
+    #     return super(CustomDataset, self).__len__()
