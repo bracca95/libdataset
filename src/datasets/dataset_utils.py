4ff40017e99f3e84b64943bdafcb3b0cf64cf926
Lorenzo Braccaioli <lbraccaioli.falcone@gmail.com>
2024-10-25 11:06:52 +0200

    mini and cifar_fs as debug datasets

--- src/datasets/dataset_utils.py
+++ src/datasets/dataset_utils.py
@@ -3,6 +3,8 @@ from torch.utils.data import Dataset
 
 from .dataset import DatasetWrapper
 from .fsl.mvtec_adac import MvtecAdac, MetaMvtec
+from .fsl.cifar import CifarFs
+from .fsl.miniimagenet import MiniImagenet
 from ..utils.config_parser import DatasetConfig
 from ..utils.tools import Logger
 
@@ -12,9 +14,19 @@ class DatasetBuilder:
     @staticmethod
     def load_dataset(dataset_config: DatasetConfig) -> Union[DatasetWrapper, Dataset]:
         if dataset_config.dataset_type == "mvtec":
+            Logger.instance().debug(f"Loading dataset MVTec")
             return MvtecAdac(dataset_config)
         elif dataset_config.dataset_type == "meta_mvtec":
+            Logger.instance().debug(f"Loading dataset meta-MVTec")
             return MetaMvtec(dataset_config, None)
+        
+        # debug
+        elif dataset_config.dataset_type == "miniimagenet":
+            Logger.instance().debug(f"Loading dataset miniimagenet")
+            return MiniImagenet(dataset_config)
+        elif dataset_config.dataset_type == "cifar_fs":
+            Logger.instance().debug(f"Loading dataset cifar_fs")
+            return CifarFs(dataset_config)
         else:
             raise ValueError(
                 "values allowed: {`mvtec`, `meta_mvtec`}"
